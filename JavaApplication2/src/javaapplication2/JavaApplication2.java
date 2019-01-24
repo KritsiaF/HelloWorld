0cfea4ad70a3e42394b35e1447c844674d41743d
KritsiaF <kritsiafigueroa@yahoo.com>
2019-01-24 14:14:43 -0500

    test

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author kfigueroa13
+ */
+public class JavaApplication2 {
+
+   public static void main(String[] args) {
+		System.out.println("Hello from the aliens of Eclipse!");
+		System.out.println("Testuuuuuuu!");
+
+	}
+
+}
